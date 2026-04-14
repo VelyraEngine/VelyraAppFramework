9f17d22d1cc6e05d78d2d7c86a9118660e7a6e8f
Arthur <arthur.preal@atlascopco.com>
2026-04-14 22:18:34 +0200

    Added extra colors

--- include/VelyraAppFramework/Styles.hpp
+++ include/VelyraAppFramework/Styles.hpp
@@ -11,5 +11,7 @@ namespace Velyra::App::Styles {
     static constexpr auto ColorRed            = ImVec4(0.8f, 0.0f, 0.0f, 1.0f);
     static constexpr auto ColorGreen          = ImVec4(0.0f, 0.8f, 0.0f, 1.0f);
     static constexpr auto ColorYellow         = ImVec4(0.8f, 0.8f, 0.0f, 1.0f);
+    static constexpr auto ColorHighLight      = ImVec4(1.0f, 230.0f / 255.0f, 120.0f / 255.0f, 125.0f/ 255.0f);
+    static constexpr auto ColorSoftHighLight  = ImVec4(1.0f, 230.0f / 255.0f, 120.0f / 255.0f, 50.0f / 255.0f);
 
 }
